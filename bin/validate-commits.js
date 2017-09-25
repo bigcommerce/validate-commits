da3857a2587a88a4969818721f36c37dc92bcb0d
Ignacio Catalina <ignacio.catalina@bigcommerce.com>
2017-09-25 16:00:30 +1000

    CHECKOUT-2306 Validate commit messages using angular format

---
+++ bin/validate-commits.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+const CommitValidator = require('../lib/commit-validator');
+const Reporter = require('../lib/reporter');
+
+const config = require('../lib/config');
+const shellRunner = require('../lib/shell-runner')
+
+const commitValidator = new CommitValidator(config);
+const reporter = new Reporter();
+
+function getNonEmptyLines(text) {
+    return text
+        .split('\n')
+        .filter(line => !line.match(/^\s*$/));
+}
+
+shellRunner.run('git log --format=%s --no-merges master..')
+    .then((commits) => {
+        const cleanCommitList = getNonEmptyLines(commits);
+        const results = commitValidator.validate(cleanCommitList);
+        reporter.printReport(results);
+
+        if (!results.valid && !process.argv.includes('--warning')) {
+            throw new Error('Commit format error!');
+        }
+    })
+    .catch(error => {
+        console.error(error.message);
+        process.exit(1);
+    });
