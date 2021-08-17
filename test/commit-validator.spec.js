8a00291f7240c11277d3a4c2068279a337c7e675
Chancellor Clark <chancellor.clark@bigcommerce.com>
2021-08-17 16:48:55 -0500

    feat(common): CHP-7101 lint messages from @commitlint

--- test/commit-validator.spec.js
+++ test/commit-validator.spec.js
@@ -2,73 +2,78 @@ const CommitValidator = require('../lib/commit-validator');
 
 describe('CommitValidator', () => {
   const options = {
-    tasks: ['feat', 'refactor'],
     scopes: ['foo', 'bar'],
   };
 
-  it('returns false if message does not contain valid task', () => {
+  it('returns false if message does not contain valid task', async () => {
     const validator = new CommitValidator(options);
 
-    expect(validator.isValid('build(foo): JIRA-1234 Add build script')).toBe(false);
-    expect(validator.isValid('docs(bar): JIRA-1234 Update docs')).toBe(false);
+    expect((await validator.isValid('build(foo): JIRA-1234 Add build script')).valid).toBe(false);
+    expect((await validator.isValid('doc(bar): JIRA-1234 Update docs')).valid).toBe(false);
   });
 
-  it('returns false if message does not contain valid scope', () => {
+  it('returns false if message does not contain valid scope', async () => {
     const validator = new CommitValidator(options);
 
-    expect(validator.isValid('refactor(hello): JIRA-1234 Extract method')).toBe(false);
-    expect(validator.isValid('feat(world): JIRA-1234 Add new feature')).toBe(false);
+    expect((await validator.isValid('refactor(hello): JIRA-1234 Extract method')).valid).toBe(
+      false,
+    );
+    expect((await validator.isValid('feat(world): JIRA-1234 Add new feature')).valid).toBe(false);
   });
 
-  it('returns false if message does not contain ticket number', () => {
+  it('returns false if message does not contain ticket number', async () => {
     const validator = new CommitValidator(options);
 
-    expect(validator.isValid('refactor(hello): Extract method')).toBe(false);
+    expect((await validator.isValid('refactor(hello): Extract method')).valid).toBe(false);
   });
 
-  it('returns true if message follows valid format', () => {
+  it('returns true if message follows valid format', async () => {
     const validator = new CommitValidator(options);
 
-    expect(validator.isValid('refactor(foo): JIRA-1234 Extract method')).toBe(true);
-    expect(validator.isValid('feat(bar): JIRA-1234 Add new feature')).toBe(true);
+    expect((await validator.isValid('refactor(foo): JIRA-1234 Extract method')).valid).toBe(true);
+    expect((await validator.isValid('feat(bar): JIRA-1234 Add new feature')).valid).toBe(true);
   });
 
-  it('returns false if message does not follow valid format', () => {
+  it('returns false if message does not follow valid format', async () => {
     const validator = new CommitValidator(options);
 
-    expect(validator.isValid('refactor[foo]: JIRA-1234 Extract method')).toBe(false);
-    expect(validator.isValid('refactor(foo): JIRA-1234: Extract method')).toBe(false);
-    expect(validator.isValid('JIRA-1234: refactor(foo) Extract method')).toBe(false);
+    expect((await validator.isValid('refactor[foo]: JIRA-1234 Extract method')).valid).toBe(false);
+    expect((await validator.isValid('refactor(foo): JIRA-1234: Extract method')).valid).toBe(false);
+    expect((await validator.isValid('JIRA-1234: refactor(foo) Extract method')).valid).toBe(false);
   });
 
-  it('returns true if message (release commit) follows valid format', () => {
+  it('returns true if message (release commit) follows valid format', async () => {
     const validator = new CommitValidator(options);
 
-    expect(validator.isValid('Releasing v1.0.0')).toBe(true);
-    expect(validator.isValid('Releasing 1.0.0')).toBe(true);
-    expect(validator.isValid('Releasing 1.0.0.rc-1')).toBe(true);
-    expect(validator.isValid('Release 1.0.0.rc-1')).toBe(true);
+    expect((await validator.isValid('Releasing v1.0.0')).valid).toBe(true);
+    expect((await validator.isValid('Releasing 1.0.0')).valid).toBe(true);
+    expect((await validator.isValid('Releasing 1.0.0.rc-1')).valid).toBe(true);
+    expect((await validator.isValid('Release 1.0.0.rc-1')).valid).toBe(true);
   });
 
-  it('returns false if message (release commit) does not follow valid format', () => {
+  it('returns false if message (release commit) does not follow valid format', async () => {
     const validator = new CommitValidator(options);
 
-    expect(validator.isValid('v1.0.0')).toBe(false);
-    expect(validator.isValid('Publishing 1.0.0')).toBe(false);
-    expect(validator.isValid('Releasing v1')).toBe(false);
+    expect((await validator.isValid('v1.0.0')).valid).toBe(false);
+    expect((await validator.isValid('Publishing 1.0.0')).valid).toBe(false);
+    expect((await validator.isValid('Releasing v1')).valid).toBe(false);
   });
 
-  it('returns true if message (revert commit) follows valid format', () => {
+  it('returns true if message (revert commit) follows valid format', async () => {
     const validator = new CommitValidator(options);
 
-    expect(validator.isValid('Revert "refactor(foo): JIRA-1234 Extract method"')).toBe(true);
-    expect(validator.isValid('Revert "Extract method"')).toBe(true);
+    expect(
+      (await validator.isValid('Revert "refactor(foo): JIRA-1234 Extract method"')).valid,
+    ).toBe(true);
+    expect((await validator.isValid('Revert "Extract method"')).valid).toBe(true);
   });
 
-  it('returns false if message (revert commit) does not follow valid format', () => {
+  it('returns false if message (revert commit) does not follow valid format', async () => {
     const validator = new CommitValidator(options);
 
-    expect(validator.isValid('Revert refactor(foo): JIRA-1234 Extract method')).toBe(false);
-    expect(validator.isValid("Revert 'Extract method'")).toBe(false);
+    expect((await validator.isValid('Revert refactor(foo): JIRA-1234 Extract method')).valid).toBe(
+      false,
+    );
+    expect((await validator.isValid("Revert 'Extract method'")).valid).toBe(false);
   });
 });
